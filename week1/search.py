ab4433184d8744d40c6735676d68f25f3023da8d
Fraser Xu <xvfeng123@gmail.com>
2022-06-11 07:30:29 +0000

    use function score

--- week1/search.py
+++ week1/search.py
@@ -1,13 +1,11 @@
 #
 # The main search hooks for the Search Flask application.
 #
-from flask import (
-    Blueprint, redirect, render_template, request, url_for
-)
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from week1.opensearch import get_opensearch
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+bp = Blueprint("search", __name__, url_prefix="/search")
 
 
 # Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display of the applied filters
@@ -17,13 +15,16 @@ bp = Blueprint('search', __name__, url_prefix='/search')
 def process_filters(filters_input):
     # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
-    display_filters = []  # Also create the text we will use to display the filters that are applied
+    display_filters = (
+        []
+    )  # Also create the text we will use to display the filters that are applied
     applied_filters = ""
     for filter in filters_input:
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName", filter)
-        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
-                                                                                 display_name)
+        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(
+            filter, filter, type, filter, display_name
+        )
         if type == "range":
             from_val = request.args.get(filter + ".from", None)
             to_val = request.args.get(filter + ".to", None)
@@ -40,25 +41,32 @@ def process_filters(filters_input):
                 to_val = "*"  # set it to * for display purposes, but don't use it in the query
             the_filter = {"range": {filter: to_from}}
             filters.append(the_filter)
-            display_filters.append("{}: {} TO {}".format(display_name, from_val, to_val))
-            applied_filters += "&{}.from={}&{}.to={}".format(filter, from_val, filter, to_val)
+            display_filters.append(
+                "{}: {} TO {}".format(display_name, from_val, to_val)
+            )
+            applied_filters += "&{}.from={}&{}.to={}".format(
+                filter, from_val, filter, to_val
+            )
         elif type == "terms":
             field = request.args.get(filter + ".fieldName", filter)
             key = request.args.get(filter + ".key", None)
             the_filter = {"term": {field: key}}
             filters.append(the_filter)
             display_filters.append("{}: {}".format(display_name, key))
-            applied_filters += "&{}.fieldName={}&{}.key={}".format(filter, field, filter, key)
+            applied_filters += "&{}.fieldName={}&{}.key={}".format(
+                filter, field, filter, key
+            )
     print("Filters: {}".format(filters))
 
     return filters, display_filters, applied_filters
 
 
-
 # Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
-@bp.route('/query', methods=['GET', 'POST'])
+@bp.route("/query", methods=["GET", "POST"])
 def query():
-    opensearch = get_opensearch() # Load up our OpenSearch client from the opensearch.py file.
+    opensearch = (
+        get_opensearch()
+    )  # Load up our OpenSearch client from the opensearch.py file.
     # Put in your code to query opensearch.  Set error as appropriate.
     error = None
     user_query = None
@@ -68,8 +76,8 @@ def query():
     filters = None
     sort = "_score"
     sortDir = "desc"
-    if request.method == 'POST':  # a query has been submitted
-        user_query = request.form['query']
+    if request.method == "POST":  # a query has been submitted
+        user_query = request.form["query"]
         if not user_query:
             user_query = "*"
         sort = request.form["sort"]
@@ -79,7 +87,9 @@ def query():
         if not sortDir:
             sortDir = "desc"
         query_obj = create_query(user_query, [], sort, sortDir)
-    elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
+    elif (
+        request.method == "GET"
+    ):  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
         filters_input = request.args.getlist("filter.name")
         sort = request.args.get("sort", sort)
@@ -99,43 +109,81 @@ def query():
 
     # print(response)
     if error is None:
-        return render_template("search_results.jinja2", query=user_query, search_response=response,
-                               display_filters=display_filters, applied_filters=applied_filters,
-                               sort=sort, sortDir=sortDir)
+        return render_template(
+            "search_results.jinja2",
+            query=user_query,
+            search_response=response,
+            display_filters=display_filters,
+            applied_filters=applied_filters,
+            sort=sort,
+            sortDir=sortDir,
+        )
     else:
         redirect(url_for("index"))
 
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
-    print("Query: {} Filters: {} Sort: {} SortDir: {}".format(user_query, filters, sort, sortDir))
+    print(
+        "Query: {} Filters: {} Sort: {} SortDir: {}".format(
+            user_query, filters, sort, sortDir
+        )
+    )
 
     sort_obj = {}
-    sort_obj[sort] = {
-        "order": sortDir
-    }
+    sort_obj[sort] = {"order": sortDir}
 
     query_obj = {
-        'size': 10,
+        "size": 10,
         "query": {
-            "bool": {
-                "must": [
-                    {
-                        "query_string": {
-                            "query": user_query,
-                            "fields": [ "name^100", "shortDescription^50", "longDescription^10" ],
-                            "phrase_slop": 3
-                        }
+            "function_score": {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "query_string": {
+                                    "query": user_query,
+                                    "fields": [
+                                        "name^100",
+                                        "shortDescription^50",
+                                        "longDescription^10",
+                                        "department",
+                                    ],
+                                    "phrase_slop": 3
+                                }
+                            }
+                        ],
+                        "filter": filters
                     }
+                },
+                "boost_mode": "replace",
+                "score_mode": "avg",
+                "functions": [
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankLongTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal",
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankMediumTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal",
+                        }
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "salesRankShortTerm",
+                            "missing": 100000000,
+                            "modifier": "reciprocal",
+                        }
+                    },
                 ],
-                "filter": filters
             }
         },
         "highlight": {
-            "fields": {
-                "name": {},
-                "shortDescription": {},
-                "longDescription": {}
-            }
+            "fields": {"name": {}, "shortDescription": {}, "longDescription": {}}
         },
         "aggs": {
             #### Step 4.b.i: create the appropriate query and aggregations here
@@ -147,46 +195,17 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
                             "key": "$",
                             "to": 100,
                         },
-                        {
-                            "key": "$$",
-                            "from": 100,
-                            "to": 200
-                        },
-                        {
-                            "key": "$$$",
-                            "from": 200,
-                            "to": 300
-                        },
-                        {
-                            "key": "$$$$",
-                            "from": 300,
-                            "to": 400
-                        },
-                        {
-                            "key": "$$$$$",
-                            "from": 400,
-                            "to": 500
-                        },
-                        {
-                            "key": "$$$$$$",
-                            "from": 500
-                        }
-                    ]
+                        {"key": "$$", "from": 100, "to": 200},
+                        {"key": "$$$", "from": 200, "to": 300},
+                        {"key": "$$$$", "from": 300, "to": 400},
+                        {"key": "$$$$$", "from": 400, "to": 500},
+                        {"key": "$$$$$$", "from": 500},
+                    ],
                 }
             },
-            "department": {
-                "terms": {
-                    "field": "department.keyword"
-                }
-            },
-            "missing_images": {
-                "missing": {
-                    "field": "image"
-                }
-            }
+            "department": {"terms": {"field": "department.keyword"}},
+            "missing_images": {"missing": {"field": "image"}},
         },
-        "sort": [
-            sort_obj
-        ]
+        "sort": [sort_obj],
     }
     return query_obj
