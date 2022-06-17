22fabdcbf561f629a21826ee000202feda2bee63
Fraser Xu <xvfeng123@gmail.com>
2022-06-17 06:03:07 +0000

    implement autocomplete

--- week2/search.py
+++ week2/search.py
@@ -57,14 +57,32 @@ def process_filters(filters_input):
 
 @bp.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    opensearch = get_opensearch()
     results = {}
     if request.method == 'GET':  # a query has been submitted
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
+            index = "bbuy_products" if type == "products" else "bbuy_queries"
+            print(f"Index: {index}")
+
+            query_obj = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion": {
+                            "field": "suggest",
+                            "skip_duplicates": "true"
+                        }
+                    }
+                }
+            }
+
             ##### W2, L3, S1
-            search_response = None
+            # search_response = None
+            search_response = opensearch.search(body=query_obj, index=index)
+
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
